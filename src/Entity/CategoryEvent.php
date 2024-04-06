32859115df943323847b61a24ad34cc03332325b
AvaKissara <94829011+AvaKissara@users.noreply.github.com>
2024-04-06 16:48:26 +0200

    ADD : class model CategoryEvent

---
+++ src/Entity/CategoryEvent.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryEventRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryEventRepository::class)]
+class CategoryEvent
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $name_category_event = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameCategoryEvent(): ?string
+    {
+        return $this->name_category_event;
+    }
+
+    public function setNameCategoryEvent(string $name_category_event): static
+    {
+        $this->name_category_event = $name_category_event;
+
+        return $this;
+    }
+}
